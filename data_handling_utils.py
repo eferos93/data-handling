c1617aa2cc70daf00ac44d8b62c60e589553fe40
eferos93 <eros.fabrici@gmail.com>
2022-01-21 10:21:10 +0100

    added python functions

---
+++ data_handling_utils.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+# given the filename of a datasheet returns a dataframe containing the [Data] in the smaple sheet
+def get_ss(file_name, tag='[Data]'):
+    with open(file_name) as in_file:
+        for line in in_file:
+            if tag in line:
+                break
+        df=pd.read_csv(in_file)
+    return df
+
+# given a dataframe with the [Data] contained in a sample sheet returns a dataframe with deduced sample names
+def get_samples(file_name):
+    df_ss = get_ss(file_name)
+    sn=df_ss["Sample_ID"].apply(lambda x : str(x))+"_S"+df_ss["I7_Index_ID"].apply(lambda x : str(int(x[3:])))
+    df_samples=pd.DataFrame(sn, columns=["sample_name"])
+    return df_samples
