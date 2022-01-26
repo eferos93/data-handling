717994092cc285c8a6bafda92581a9c16f9049e3
eferos93 <eros.fabrici@gmail.com>
2022-01-26 14:46:16 +0100

    added code for generating bcl2fastq output file names

--- data_handling_utils.py
+++ data_handling_utils.py
@@ -1,5 +1,7 @@
+from itertools import product
 import pandas as pd
 import sys
+import json
 
 # given the filename of a datasheet returns a dataframe containing the [Data] in the smaple sheet
 def get_ss(file_name, tag='[Data]'):
@@ -7,15 +9,26 @@ def get_ss(file_name, tag='[Data]'):
         for line in in_file:
             if tag in line:
                 break
-        df=pd.read_csv(in_file)
+        df = pd.read_csv(in_file)
     return df
 
 # given a dataframe with the [Data] contained in a sample sheet returns a dataframe with deduced sample names
 def get_samples(file_name):
     df_ss = get_ss(file_name)
-    sn=df_ss["Sample_ID"].apply(lambda x : str(x))+"_S"+df_ss["I7_Index_ID"].apply(lambda x : str(int(x[3:])))
-    df_samples=pd.DataFrame(sn, columns=["sample_name"])
+    sn = df_ss["Sample_ID"].apply(lambda x : str(x))+"_S" + df_ss["I7_Index_ID"].apply(lambda x : str(int(x[3:])))
+    df_samples = pd.DataFrame(sn, columns=["sample_name"])
     return df_samples
 
 # argv[0] is the path to the sample_sheet, argv[1] the output path
-get_samples(sys.argv[0]).to_csv(sys.argv[1], index=False)
+df_samples = get_samples(sys.argv[0])
+df_samples.to_csv(sys.argv[1], index=False)
+
+# get the names of the fastq files that will be produced, will be used then by fastqc
+fastq_names = []
+for row in df_samples.reset_index().itertuples():
+    temp = {}
+    for i in ["1", "2"]:
+        temp["file" + i] = row.sample_name + "_R" + i + "_001.fastq.gz"
+    fastq_names.append(temp)
+
+json.dump(fastq_names, sys.stdout)
