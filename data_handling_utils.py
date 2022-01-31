60bb64d8a4ccf5dc757ac73575ee34bd75106663
eferos93 <eros.fabrici@gmail.com>
2022-01-31 17:01:54 +0100

    enhanchment

--- data_handling_utils.py
+++ data_handling_utils.py
@@ -12,12 +12,26 @@ def get_ss(file_name, tag='[Data]'):
         df = pd.read_csv(in_file)
     return df
 
+def build_sample_name(df_sample_sheet):
+    if df_sample_sheet["Sample_Name"].isnull().all():
+        sample_names = df_sample_sheet["Sample_ID"].apply(lambda x : str(x)) + "_S" + df_sample_sheet["I7_Index_ID"].apply(lambda x : str(int(x[3:])))
+        if not df_sample_sheet["Sample_Project"].isnull().all():
+            sample_names = df_sample_sheet["Sample_Project"] + "/" + sample_names
+
+        return pd.DataFrame(sample_names, columns=["Sample_Name"])   
+    else:
+        sample_names = df_sample_sheet["Sample_Name"]
+        if not df_sample_sheet["Sample_Project"].isnull().all():
+            sample_names = df_sample_sheet["Sample_Project"] + "/" + df_sample_sheet["Sample_Name"]
+        return pd.DataFrame(sample_names, columns=["Sample_Name"])
+
 # given a dataframe with the [Data] contained in a sample sheet returns a dataframe with deduced sample names
 def get_samples(file_name):
     df_ss = get_ss(file_name)
-    sn = df_ss["Sample_ID"].apply(lambda x : str(x))+"_S" + df_ss["I7_Index_ID"].apply(lambda x : str(int(x[3:])))
-    df_samples = pd.DataFrame(sn, columns=["sample_name"])
-    return df_samples
+    # sn = df_ss["Sample_ID"].apply(lambda x : str(x)) + "_S" + df_ss["I7_Index_ID"].apply(lambda x : str(int(x[3:])))
+    # df_samples = pd.DataFrame(sn, columns=["Sample_Name"])
+    # return df_samples
+    return build_sample_name(df_ss)
 
 # argv[1] is the path to the sample_sheet, argv[2] the output path, argv[3] the filename
 df_samples = get_samples(sys.argv[1])
