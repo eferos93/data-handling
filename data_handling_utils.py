34d5834685759e6b29989fd244cf0e7b7f1ac14f
eferos93 <eros.fabrici@gmail.com>
2022-02-06 22:38:20 +0100

    fix

--- data_handling_utils.py
+++ data_handling_utils.py
@@ -73,4 +73,4 @@ for row in df_samples.reset_index().itertuples():
         temp["file" + i] = row.Sample_Name + "_R" + i + "_001.fastq.gz"
     fastq_names.append(temp)
 
-json.dump({"sample-sheet-path": sampleSheetPath, "fastq-names": fastq_names}, sys.stdout)
+json.dump({"sample-sheet-path": sampleSheetPath, "fastq-names": json.dumps(fastq_names)}, sys.stdout)
