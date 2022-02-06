7473ccee2dff6220d71568f935dbd52ddcc97d77
eferos93 <eros.fabrici@gmail.com>
2022-02-06 22:09:28 +0100

    removed unused function

--- data_handling_utils.py
+++ data_handling_utils.py
@@ -42,7 +42,6 @@ def find(pattern, files):
     return ""
 
 def wait_sample_sheet(input_dir):
-
     sampleSheetPath = find('*.csv', os.listdir(input_dir))
     while sampleSheetPath == "":
         info("Sample Sheet not found yet...")
@@ -51,45 +50,15 @@ def wait_sample_sheet(input_dir):
         
     return sampleSheetPath
 
-def walklevel(path, depth = 1):
-    """It works just like os.walk, but you can pass it a level parameter
-       that indicates how deep the recursion will go.
-       If depth is 1, the current directory is listed.
-       If depth is 0, nothing is returned.
-       If depth is -1 (or less than 0), the full depth is walked.
-    """
-    # If depth is negative, just walk
-    # Not using yield from for python2 compat
-    # and copy dirs to keep consistant behavior for depth = -1 and depth = inf
-    # if depth < 0:
-    #     for root, dirs, files in os.walk(path):
-    #         yield root, dirs[:], files
-    #     return
-    # elif depth == 0:
-    #     return
-
-    # path.count(os.path.sep) is safe because
-    # - On Windows "\\" is never allowed in the name of a file or directory
-    # - On UNIX "/" is never allowed in the name of a file or directory
-    # - On MacOS a literal "/" is quitely translated to a ":" so it is still
-    #   safe to count "/".
-    base_depth = path.rstrip(os.path.sep).count(os.path.sep)
-    for root, dirs, files in os.walk(path):
-        yield root, dirs[:], files
-        cur_depth = root.count(os.path.sep)
-        if base_depth + depth <= cur_depth:
-            del dirs[:]
 
 
 # argv[1] is the path to the input_dir, argv[2] the output path, argv[3] the filename
-info("Starting to look for the sample sheet")
-info(sys.argv[1])
 
+info("Starting to look for the sample sheet")
 sampleSheetName = wait_sample_sheet(sys.argv[1])
 if sampleSheetName == "":
     raise Exception("No Sample Sheet Provided!")
 
-info(sampleSheetName)
 sampleSheetPath = os.path.join(sys.argv[1], sampleSheetName)
 df_samples = get_samples(sampleSheetPath)
 output_path = Path(sys.argv[2])
