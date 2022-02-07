196c1d2015aade8bba70b4e73eb65bca7c883e4f
eferos93 <eros.fabrici@gmail.com>
2022-02-07 18:59:54 +0100

    added wrap

--- tests.py
+++ tests.py
@@ -2,6 +2,7 @@ from distutils.command.build import build
 import io
 import unittest
 import unittest.mock
+from textwrap import wrap
 
 from data_handling_utils import build_fastq_names
 
@@ -36,7 +37,7 @@ class TestDataHandling(unittest.TestCase):
     @unittest.mock.patch('sys.stdout', new_callable=io.StringIO)
     def assert_stdout(self, input_dir, output_path, samples_filename, expected_output, mock_stdout):
         build_fastq_names(input_dir, output_path, samples_filename)
-        self.assertEqual(mock_stdout.getvalue(), expected_output)
+        self.assertEqual(wrap(mock_stdout.getvalue()), wrap(expected_output))
 
     def test_data_handling(self):
         self.assert_stdout('test_folder_2', 'test_folder_2/output/', 'samples.csv', expected_output2)
